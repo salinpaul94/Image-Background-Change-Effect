3b9f88e54a98987cb56fa40ab1874979f5b0c3fd
salinpaul valooran <salinpv@hotmail.com>
2023-11-04 20:36:41 +0530

    getting time difference b/w launch date and current time

--- version_01/index.js
+++ version_01/index.js
@@ -1,4 +1,5 @@
 let countDownDate = new Date("Dec 1, 2023 00:00:00").getTime();
 let x = setInterval(function () {
   let now = new Date().getTime();
+  let distance = countDownDate - now;
 }, 1000);
