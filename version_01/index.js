50313f1c7ec4a11a4a0f40fc929cf8459f093689
salinpaul valooran <salinpv@hotmail.com>
2023-11-04 20:37:49 +0530

    calculating days to launch

--- version_01/index.js
+++ version_01/index.js
@@ -2,4 +2,6 @@ let countDownDate = new Date("Dec 1, 2023 00:00:00").getTime();
 let x = setInterval(function () {
   let now = new Date().getTime();
   let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
 }, 1000);
